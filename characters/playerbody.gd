fd0c9f6aeb4ff132e471d4e199ad506dde456a15
huttonb <huttonb@mail.gvsu.edu>
2022-10-15 19:58:57 -0400

    added basic shadow monster

--- characters/playerbody.gd
+++ characters/playerbody.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 @export var bullet: PackedScene
 
-const SPEED = 300.0
+const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
 
 var flipped = false: set = _flip_character
+
+@onready var stateMachine = %AnimationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -13,6 +15,15 @@ func _physics_process(delta):
 	var new_velocity = direction * SPEED
 	velocity = new_velocity
 	move_and_slide()
+	if velocity.length() > 1:
+		stateMachine.travel("Walk")
+		print("walk")
+	elif velocity.length() < -1:
+		stateMachine.travel("R_Walk")
+		print("rwalk")
+	elif(stateMachine.get_current_node() == "Walk" || stateMachine.get_current_node() == "R_Walk"):
+		stateMachine.travel("Idle")
+		print("idle")
 	_look(delta)
 	
 	
@@ -38,9 +49,11 @@ func _look(delta):
 	%RightArmSprite.global_position = %Shotgun/GunSprite/Pump.global_position
 	%RightArmSprite.look_at(%RightShoulder.global_position) 
 	%RightArmSprite.rotate(PI)
-	#%Shotgun.look_at(get_global_mouse_position())
+	%HeadSprite.look_at(get_global_mouse_position())
+
 	
 func _flip_character(flip):
 	var x_axis = global_transform.x
 	global_transform.x.x = (-1 if flip else 1) * abs(x_axis.x)
 	flipped = flip
+
