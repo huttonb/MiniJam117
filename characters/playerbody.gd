916c537016923d958e4f9bd17408aaf0ef7281c6
huttonb <huttonb@mail.gvsu.edu>
2022-10-15 14:19:04 -0400

    basic animations and shotgun sound effect

--- characters/playerbody.gd
+++ characters/playerbody.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var flipped = false
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
@@ -22,8 +24,15 @@ func _shoot_shotgun():
 	var b = bullet.instantiate()
 	owner.add_child(b)
 	b.transform = %Shotgun/Muzzle.global_transform
+	%Shotgun/GunSprite/ShotgunShotSound.play()
+	
 	
 func _look(delta):
+	var lookPos = (get_local_mouse_position().x > 0)
+	if (lookPos && !flipped) || (lookPos && flipped) :
+		_flip_character(true)
+	else:
+		_flip_character(false)
 	%LeftArmSprite.look_at(get_global_mouse_position())
 	#%RightArmSprite.look_at(self.position)
 	%RightArmSprite.global_position = %Shotgun/GunSprite/Pump.global_position
@@ -31,3 +40,6 @@ func _look(delta):
 	%RightArmSprite.rotate(PI)
 	#%Shotgun.look_at(get_global_mouse_position())
 	
+func _flip_character(flip):
+	var x_axis = global_transform.x
+	global_transform.x.x = (-1 if flip else 1) * abs(x_axis.x)
