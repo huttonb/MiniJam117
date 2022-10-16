a102bf3ad28cc5d6937f5832881b8b14da763083
johnsdjustin <johnsdjustin@gmail.com>
2022-10-16 15:29:30 -0400

    Creating a rough, rough draft of the clip system. This will need to be revisited

--- bullets/bullets.gd
+++ bullets/bullets.gd
@@ -3,16 +3,18 @@ extends Area2D
 class_name Bullet
 
 var travel_speed: int
+@onready var timer = get_tree().create_timer(10).timeout
 
 func _constructor():
 	travel_speed = 800
 
 # Called when the node enters the scene tree for the first time.
-func _ready ():
+func _ready():
 	_clean()
-	
+
+# TODO: Check if memory has already been freed before calling queue_free	
 func _clean():
-	await get_tree().create_timer(10).timeout
+	await timer
 	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,5 +24,4 @@ func _physics_process(delta):
 func _on_bullet_body_entered(body):
 	if body.is_in_group("enemies"): # TODO
 		body.kill_enemy() # TODO
-	queue_free()
-	
+	timer.emit()
